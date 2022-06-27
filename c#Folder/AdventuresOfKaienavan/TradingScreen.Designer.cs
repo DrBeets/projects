8b5bd1438078f28705045725b9f3b05456a20370
Tristan Rogers <108250098+DrBeets@users.noreply.github.com>
2022-06-27 00:13:24 -0400

    Add files via upload

---
+++ c#Folder/AdventuresOfKaienavan/TradingScreen.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace AdventuresOfKaienavan
+{
+    partial class TradingScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMyInventory = new System.Windows.Forms.Label();
+            this.lblVendorInventory = new System.Windows.Forms.Label();
+            this.dgvMyItems = new System.Windows.Forms.DataGridView();
+            this.dgvOrindaeItems = new System.Windows.Forms.DataGridView();
+            this.btnClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMyItems)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrindaeItems)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblMyInventory
+            // 
+            this.lblMyInventory.AutoSize = true;
+            this.lblMyInventory.Location = new System.Drawing.Point(99, 13);
+            this.lblMyInventory.Name = "lblMyInventory";
+            this.lblMyInventory.Size = new System.Drawing.Size(68, 13);
+            this.lblMyInventory.TabIndex = 0;
+            this.lblMyInventory.Text = "My Inventory";
+            // 
+            // lblVendorInventory
+            // 
+            this.lblVendorInventory.AutoSize = true;
+            this.lblVendorInventory.Location = new System.Drawing.Point(349, 13);
+            this.lblVendorInventory.Name = "lblVendorInventory";
+            this.lblVendorInventory.Size = new System.Drawing.Size(98, 13);
+            this.lblVendorInventory.TabIndex = 1;
+            this.lblVendorInventory.Text = "Orindae\'s Inventory";
+            // 
+            // dgvMyItems
+            // 
+            this.dgvMyItems.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMyItems.Location = new System.Drawing.Point(13, 43);
+            this.dgvMyItems.Name = "dgvMyItems";
+            this.dgvMyItems.Size = new System.Drawing.Size(240, 216);
+            this.dgvMyItems.TabIndex = 2;
+            // 
+            // dgvOrindaeItems
+            // 
+            this.dgvOrindaeItems.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvOrindaeItems.Location = new System.Drawing.Point(276, 43);
+            this.dgvOrindaeItems.Name = "dgvOrindaeItems";
+            this.dgvOrindaeItems.Size = new System.Drawing.Size(240, 216);
+            this.dgvOrindaeItems.TabIndex = 3;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(227, 275);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // TradingScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 310);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.dgvOrindaeItems);
+            this.Controls.Add(this.dgvMyItems);
+            this.Controls.Add(this.lblVendorInventory);
+            this.Controls.Add(this.lblMyInventory);
+            this.Name = "TradingScreen";
+            this.Text = "Oridae\'s Goods";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMyItems)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrindaeItems)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMyInventory;
+        private System.Windows.Forms.Label lblVendorInventory;
+        private System.Windows.Forms.DataGridView dgvMyItems;
+        private System.Windows.Forms.DataGridView dgvOrindaeItems;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
