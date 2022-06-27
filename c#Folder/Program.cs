43e40a0f8aa17b30c4189292af47ce0ce2be3772
Tristan Rogers <108250098+DrBeets@users.noreply.github.com>
2022-06-27 00:16:29 -0400

    Add files via upload

---
+++ c#Folder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Bands
+{/* This is a VERY simple program that displays three of my favorite musicians/bands.
+  * Written by Tristan Rogers */
+    class Bands
+    {
+        static void Main(string[] args)
+        {
+            //Writes each group on a seperate line
+            Console.WriteLine("My favorite band is Affiance");
+            Console.WriteLine("My second favorite band is Dance Gavin Dance");
+            Console.WriteLine("My favorite performer goes by the name of Reol.");
+            
+        }//End of Main
+    }
+}
