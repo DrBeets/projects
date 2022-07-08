f0ac2b87fd07d20ad15b21fb852917ec35833dd1
Tristan Rogers <108250098+DrBeets@users.noreply.github.com>
2022-07-08 14:05:43 -0400

    Add files via upload

---
+++ c#Folder/Program.cs
@@ -0,0 +1,40 @@
+﻿/* This program is designed to take a number that the user inputs to check if they won the lottery
+ * Written by Tristan Rogers 2021
+ */
+
+using System;
+
+namespace Lotteria
+{
+    class Lotteria
+    {
+        static void Main(string[] args)
+        {
+            // Create array and variables
+            int[] winningNumbers = { 2, 19, 32, 36, 50, 52 };
+            int numEntered;
+            bool isWinningNumber = false;
+
+            //Prompt user for number input
+            Console.WriteLine("Please enter your SINGLE lottery number");
+            numEntered = Convert.ToInt32(Console.ReadLine());
+
+            // Loop to search array for matching number
+            for(int x = 0; x < winningNumbers.Length; ++x)
+            {
+                if (numEntered == winningNumbers[x])
+                    isWinningNumber = true;
+            }
+
+            // Output 
+            Console.WriteLine("The date of this lottery drawing is 03/06/2021.");
+            if (isWinningNumber)
+                Console.WriteLine("Winner! Your number matches!");
+            else
+                Console.WriteLine("Loser! Better luck next time!");
+            
+            // Keeps window open
+            Console.ReadKey();
+        }
+    }
+}
