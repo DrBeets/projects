de26f7d2fcbdfbda6074020b4b47a1496559bed6
Tristan Rogers <108250098+DrBeets@users.noreply.github.com>
2022-07-08 14:12:14 -0400

    Add files via upload

---
+++ PythonFolder/Small birds.py
@@ -0,0 +1,8 @@
+#Program that displays the world's five smallest birds.
+#Written by Tristan Rogers
+print("The Bee Hummingbird is the world's smallest bird at 1.7 grams.")
+print("The second smallest bird is Costa's Hummingbird weighing 3 grams.")
+print("The third smallest bird is the Weebil weighing 6 grams.")
+print("The fourth smallest bird is the Goldcrest weighing 5.7 grams.")
+print("The fifth smallest bird is the Simple Firecrest weighing 5.5 grams.")
+print("This information is provided by safariisafricana.com.")
