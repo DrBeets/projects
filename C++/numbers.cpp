d87895de4dcddf2274a5a27487c8c80ccb3bf075
Tristan Rogers <108250098+DrBeets@users.noreply.github.com>
2025-10-23 19:04:36 -0400

    Update numbers.cpp

--- C++/numbers.cpp
+++ C++/numbers.cpp
@@ -4,4 +4,11 @@ int main()
 {
   int num1, num2;
   std::cout << "Enter an integer: \n";
+  std::cin >> num1;
+
+  std::out << "Enter another integer: \n";
+  std::cin >> num2;
+
+  std::out << num1 << " + " << num2 << " is " << num1 + num2;
+  std::out << num1 << " - " << num2 << " is " << num1 - num2;
 }  
